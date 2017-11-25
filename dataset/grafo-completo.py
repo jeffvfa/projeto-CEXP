53522114e4a88ced6dffbf102794654bfa3d530e
andregiampaolo <andregiampaolo@gmail.com>
2017-11-25 18:52:27 -0200

    Alterando estrutura do arquivo gerado

--- dataset/grafo-completo.py
+++ dataset/grafo-completo.py
@@ -1,14 +1,14 @@
 from random import randint
 
 # quantidade de nós/vertices do grafo 
-quantity_node = 100
+quantity_node = 10000
 # peso mínimo de uma aresta
 min_range_weight = 0
 # peso máximo de uma aresta
 max_range_weight = 10
 
 # criando arquivo
-f= open("graph.txt","w+")
+f= open("grafo-completo-10000.txt","w+")
 
 # escrevendo a quantidade de vertices
 f.write("%d\n" % quantity_node)
@@ -17,7 +17,7 @@ f.write("%d\n" % quantity_node)
 for x in range(0, quantity_node):
 	for y in range((x+1), quantity_node):
 		range_weight = randint(min_range_weight, max_range_weight)
-		f.write("%d,%d,%d\n" % (x,y,range_weight))
-		print ("valor da variavel (%d,%d,%d) " % (x,y,range_weight))
+		f.write("%d %d %d\n" % (x,y,range_weight))
+		print ("Vertice: (%d,%d,%d) " % (x,y,range_weight))
 
 f.close() 
--- dataset/grafo-esparso.py
+++ dataset/grafo-esparso.py
@@ -1,14 +1,14 @@
 from random import randint
 
 # quantidade de nós/vertices do grafo 
-quantity_node = 100
+quantity_node = 10000
 # peso mínimo de uma aresta
 min_range_weight = 0
 # peso máximo de uma aresta
 max_range_weight = 10
 
 # criando arquivo
-f = open("graph-esparso.txt","w+")
+f = open("grafo-esparso-10000.txt","w+")
 
 # escrevendo a quantidade de vertices
 f.write("%d\n" % quantity_node)
@@ -20,8 +20,8 @@ x = 0
 while x < (quantity_node-1):
 	range_weight = randint(min_range_weight, max_range_weight)
 	y = x + 1
-	f.write("%d,%d,%d\n" % (x,y,range_weight))
-	print ("valor da variavel (%d,%d,%d) " % (x,y,range_weight))
+	f.write("%d %d %d\n" % (x,y,range_weight))
+	print ("Vertices: (%d,%d,%d) " % (x,y,range_weight))
 	x = x + 1
 		
 f.close() 
